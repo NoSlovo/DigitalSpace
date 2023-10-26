69e1f762e1434b0389cacb37b75374492434e79c
Mirage <zherebchikov456@gmail.com>
2023-10-27 02:52:09 +0500

    Normalaize

--- Assets/_Project/_Scripts/Player/Animator/CharacterAnimator.cs
+++ Assets/_Project/_Scripts/Player/Animator/CharacterAnimator.cs
@@ -5,10 +5,11 @@ namespace _Project.Scripts.Player.CharacterAnimator
     public class CharacterAnimator : MonoBehaviour
     {
         [SerializeField] private Animator _animatorCharacter;
-        
+
         private static readonly int _iJump = Animator.StringToHash("IJump");
         private static readonly int _speed = Animator.StringToHash("Speed");
         private const string _nameAnimation = "Jump";
+
         public void Jump()
         {
             _animatorCharacter.Play(_nameAnimation);
@@ -18,9 +19,8 @@ namespace _Project.Scripts.Player.CharacterAnimator
         {
             if (speed >= 0)
             {
-                _animatorCharacter.SetFloat(_speed,speed);
+                _animatorCharacter.SetFloat(_speed, speed);
             }
         }
-        
     }
 }
